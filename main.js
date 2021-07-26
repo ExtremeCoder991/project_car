76a941083c65f82eda909407264bcc9afbe427b3
ExtremeCoder991 <75404903+ExtremeCoder991@users.noreply.github.com>
2021-07-26 15:18:29 -0700

    Update main.js

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ function add() {
     car1_imgTag.onload = uploadcar1;
     car1_imgTag.src = car1_image;
 
-    
+
     car2_imgTag = new Image();
     car2_imgTag.onload = uploadcar1;
     car2_imgTag.src = car2_image;
@@ -44,48 +44,129 @@ function uploadcar2() {
 
 window.addEventListener("keydown", my_keydown);
 
-function my_keydown(e)
-{
+function my_keydown(e) {
     keyPressed = e.keyCode;
     console.log(keyPressed);
-        if(keyPressed == '38')
-        {
-            car1_up();
-            console.log("up arrow key");
-        }
-        if(keyPressed == '40')
-        {
-            car1_down();
-            console.log("down arrow key");
-        }
-        if(keyPressed == '37')
-        {
-            car1_left();
-            console.log("left arrow key");
-        }
-        if(keyPressed == '39')
-        {
-            car1_right();
-            console.log("right arrow key");
-        }
-        if(keyPressed == '87')
-        {
-            car2_up();
-            console.log("key w");
-        }
-        if(keyPressed == '65')
-        {
-            car2_left();
-            console.log("key a");
-        }
-        if(keyPressed == '83')
-        {
-            car2_down();
-            console.log("key s");
-        }
-        if(keyPressed == '68')
-        {
-            car2_right();
-            console.log("key d");
-        }
+    if (keyPressed == '38') {
+        car1_up();
+        console.log("up arrow key");
+    }
+    if (keyPressed == '40') {
+        car1_down();
+        console.log("down arrow key");
+    }
+    if (keyPressed == '37') {
+        car1_left();
+        console.log("left arrow key");
+    }
+    if (keyPressed == '39') {
+        car1_right();
+        console.log("right arrow key");
+    }
+    if (keyPressed == '87') {
+        car2_up();
+        console.log("key w");
+    }
+    if (keyPressed == '65') {
+        car2_left();
+        console.log("key a");
+    }
+    if (keyPressed == '83') {
+        car2_down();
+        console.log("key s");
+    }
+    if (keyPressed == '68') {
+        car2_right();
+        console.log("key d");
+    }
+}
+
+function car1_up() {
+    if (car1_y >= 0) {
+        car1_y = car1_y - 10;
+        console.log("When up arrow is pressed,  x = " + car1_x + " | y = " + car1_y);
+        uploadBackground();
+        uploadcar1();
+        uploadcar2();
+    }
+}
+
+function car1_down() {
+    if (car1_y <= 500) {
+        car1_y = car1_y + 10;
+        console.log("When down arrow is pressed,  x = " + car1_x + " | y = " + car1_y);
+        uploadBackground();
+        uploadcar1();
+        uploadcar2();
+    }
+}
+
+function car1_left() {
+    if (car1_x >= 0) {
+        car1_y = car1_y - 10;
+        console.log("When left arrow is pressed,  x = " + car1_x + " | y = " + car1_y);
+        uploadBackground();
+        uploadcar1();
+        uploadcar2();
+    }
+}
+
+function car1_right() {
+    if (car1_y <= 500) {
+        car1_y = car1_y + 10;
+        console.log("When right arrow is pressed,  x = " + car1_x + " | y = " + car1_y);
+        uploadBackground();
+        uploadcar1();
+        uploadcar2();
+    }
+}
+
+function car2_up() {
+    if (car2_y >= 0) {
+        car2_y = car2_y - 10;
+        console.log("When up arrow is pressed,  x = " + car2_x + " | y = " + car2_y);
+        uploadBackground();
+        uploadcar1();
+        uploadcar2();
+    }
+}
+
+function car2_down() {
+    if (car2_y <= 500) {
+        car2_y = car2_y + 10;
+        console.log("When down arrow is pressed,  x = " + car2_x + " | y = " + car2_y);
+        uploadBackground();
+        uploadcar1();
+        uploadcar2();
+    }
+}
+
+function car2_left() {
+    if (car2_x >= 0) {
+        car2_y = car2_y - 10;
+        console.log("When left arrow is pressed,  x = " + car2_x + " | y = " + car2_y);
+        uploadBackground();
+        uploadcar1();
+        uploadcar2();
+    }
+}
+
+function car2_right() {
+    if (car2_y <= 500) {
+        car2_y = car2_y + 10;
+        console.log("When right arrow is pressed,  x = " + car2_x + " | y = " + car2_y);
+        uploadBackground();
+        uploadcar1();
+        uploadcar2();
+    }
+}
+
+if (car1_x > 700) {
+    console.log("car1 Won");
+    document.getElementById('game_status').innerHTML = "Car 1 Won!!!";
+}
+
+if (car2_x > 700) {
+    console.log("car2 Won");
+    document.getElementById('game_status').innerHTML = "Car 2 Won!!!";
 }
